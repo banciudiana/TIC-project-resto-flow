663b6b74b19230c0152ad92bbb8ef69604f5c907
banciudiana <dianaelenabanciu@gmail.com>
2026-01-05 14:55:02 +0200

    1. express project initialization

--- server/app.js
+++ server/app.js
@@ -1,26 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
 require('dotenv').config();
+const express = require('express');
+const globalAppMiddleware = require('./middleware/globalAppMiddleware');
+
+const app = express();
+globalAppMiddleware(app);
 
 //ROUTES
 const authRoutes = require('./routes/authRoute');
 
-const app = express();
-
-app.use(helmet());
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-
-
-
 
 app.use('/api/auth', authRoutes);
+//check logging system for cors, helmet, morgan
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Logging system active' });
+});
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(` Server is running on port ${PORT}`)
-);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(` Server is running on port ${PORT}`);
+  console.log(` Mod: ${process.env.NODE_ENV}`);
+});
 
---
+++ server/middleware/globalAppMiddleware.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+module.exports = (app) => {
+  app.use(helmet());
+  app.use(cors());
+  app.use(morgan('dev'));
+  app.use(express.json());
+};
